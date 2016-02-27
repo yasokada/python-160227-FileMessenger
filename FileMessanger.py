f69d0c5f8a46bfc0a493051e869cc0942f0dd07a
yasokada <yasokada@gmail.com>
2016-02-27 18:03:48 +0900

    add msgr_filecopy()

--- FileMessanger.py
+++ FileMessanger.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 
 '''
+v0.2 2016 Feb. 27
+    - add msgr_filecopy()
 v0.1 2016 Feb. 27
     - just hello
 '''
 
-print "hello"
+import os.path
+import shutil
+
+pizeropath="/home/pi/BYOP"
+usbpath="/media/BYOP"
+
+def msgr_filecopy(srcpath, dstpath):
+    print "myUpdate"
+    if os.path.isdir(usbpath)==False:
+        print "no file"
+        return
+    shutil.copyfile(srcpath, dstpath)    
+
+srcname=usbpath + "/" + "name.txt"
+dstname=pizeropath + "/" + "name.txt"
+print srcname
+
+msgr_filecopy(srcname, dstname)
+
+    
